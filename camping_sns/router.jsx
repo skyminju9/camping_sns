1a630352f65c4caf3c9eec467e77e24beb6df0d7
skyminju9 <skyminju9@naver.com>
2024-04-18 14:53:58 +0900

    feat : router

--- camping_sns/router.jsx
+++ camping_sns/router.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Splash from './src/pages/Splash';
-import {MainTab} from './src/tabs/MainTab';
+import LoginScreen from './src/pages/LoginScreen';
+import OnBoardingScreen from './src/pages/OnBoardingScreen';
 import HomeScreen from './src/pages/HomeScreen';
+import SignUpScreen from './src/pages/SignUpScreen';
+import FindPasswordScreen from './src/pages/FindPasswordScreen';
+import MainTab from './src/tabs/MainTab';
+import ArticleDetailScreen from './src/pages/ArticleDetailScreen';
 
 const Stack = createStackNavigator();
 
@@ -12,7 +17,15 @@ const Router = () => {
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Splash" component={Splash} />
       <Stack.Screen name="MainTab" component={MainTab} />
+      <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
+      <Stack.Screen name="FindPasswordScreen" component={FindPasswordScreen} />
+      <Stack.Screen
+        name="ArticleDetailScreen"
+        component={ArticleDetailScreen}
+      />
     </Stack.Navigator>
   );
 };
